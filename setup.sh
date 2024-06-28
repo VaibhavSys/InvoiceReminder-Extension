8d7ede338a7c80cd7c25e9cbe9b0096d8d7d62c5
VaibhavSys <vaibhavsys@protonmail.com>
2024-06-28 23:03:32 +0530

    Update setup.sh script to use a temporary patch file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -o index.blade.php.patch https://github.com/VaibhavSys/InvoiceReminder-Extension/raw/master/index.blade.php.patch
+curl -o tmp-index.blade.php.patch https://github.com/VaibhavSys/InvoiceReminder-Extension/raw/master/index.blade.php.patch
 patch themes/default/views/admin/invoices/index.blade.php < index.blade.php.patch
+rm tmp-index.blade.php.patch
